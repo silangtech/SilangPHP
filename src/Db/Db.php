b658849416e5321960323bfc43e37e19251121cf
show <showshou@126.com>
2021-05-18 11:10:10 +0800

    增加laravel Db facade

---
+++ src/Db/Db.php
@@ -0,0 +1,28 @@
+<?php
+/*LICENSE
++-----------------------------------------------------------------------+
+| SilangPHP Framework                                                   |
++-----------------------------------------------------------------------+
+| This program is free software; you can redistribute it and/or modify  |
+| it under the terms of the GNU General Public License as published by  |
+| the Free Software Foundation. You should have received a copy of the  |
+| GNU General Public License along with this program.  If not, see      |
+| http://www.gnu.org/licenses/.                                         |
+| Copyright (C) 2020. All Rights Reserved.                              |
++-----------------------------------------------------------------------+
+| Supports: http://www.github.com/silangtech/SilangPHP                  |
++-----------------------------------------------------------------------+
+*/
+declare(strict_types=1);
+namespace SilangPHP\Db;
+use Illuminate\Database\Capsule\Manager;
+
+/**
+ * Class Db
+ * 解决引用的问题，单例query
+ * @package support
+ */
+class Db extends Manager
+{
+
+}
